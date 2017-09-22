17c2421765af12f972ce44df60fcfed7b4b75d39
Thomas Lindstrøm <t@hom.as>
2017-09-22 12:40:02 +0200

    Avoid direct comparison of string literal

--- src/ios/HijackWebviewLinkClick.m
+++ src/ios/HijackWebviewLinkClick.m
@@ -28,7 +28,7 @@
 
     - (bool) shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType {
         if (navigationType == UIWebViewNavigationTypeLinkClicked) {
-            if ([self getListenerId] != "") {
+            if ([[self getListenerId] isEqualToString:@""]) {
                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[request URL] absoluteString]];
 
                 [pluginResult setKeepCallbackAsBool:YES];
